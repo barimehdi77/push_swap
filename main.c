5a54aed38f1d7a29bdc631ade2ab0bfe6e295b71
Mehdi Bari <mbari@e2r3p2.1337.ma>
2021-06-08 19:50:00 +0100

    rename main.c

--- main.c
+++ main.c
@@ -5,5 +5,9 @@
 int main(int ac, char **av)
 {
 	int *arr;
+	int i;
 	// create function gets the numbers in the arguments and stroe them in arr
+	i = 0;
+	while (av[i])
+		printf("|%s|\n", av[i++]);
 }
