04e5e28a499deac2a771873b8ffd69d15f17f07b
barimehdi77 <barimehdi777@gmail.com>
2021-06-09 21:10:37 +0100

    create some instructions (sa, sb, ss, pa, pb, ra, rb) and create a print functions

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 20:55:13 by mbari             #+#    #+#             */
-/*   Updated: 2021/06/08 21:18:41 by mbari            ###   ########.fr       */
+/*   Updated: 2021/06/09 21:07:26 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ int	ft_isnumber(char *number)
 
 int	ft_put_err(t_stacks *stacks, char *message, int ret)
 {
-	free(stacks->stack_a);
-	free(stacks->stack_b);
+	free(stacks->stack_a.vector);
+	free(stacks->stack_b.vector);
 	if (message != NULL)
 		ft_putendl_fd(message, 2);
 	return (ret);
@@ -43,10 +43,10 @@ int ft_isduplicate(t_stacks *stacks, int number)
 {
 	int size;
 
-	size = stacks->arr_size;
+	size = stacks->stack_a.used_size;
 	while (size >= 0)
 	{
-		if (stacks->stack_a[size] == number)
+		if (stacks->stack_a.vector[size] == number)
 			return (0);
 		size--;
 	}
@@ -60,9 +60,12 @@ t_stacks	ft_store_numbers(int size, char **av)
 	int			i;
 
 	i = 0;
-	stacks.stack_a = (int *)malloc(sizeof(int) * size);
-	stacks.stack_b = (int *)malloc(sizeof(int) * size);
-	stacks.arr_size = size;
+	stacks.stack_a.vector = (int *)malloc(sizeof(int) * size);
+	stacks.stack_b.vector = (int *)malloc(sizeof(int) * size);
+	stacks.stack_a.size = size;
+	stacks.stack_b.size = size;
+	stacks.stack_a.used_size = 0;
+	stacks.stack_b.used_size = 0;
 	while (*av)
 	{
 		if (!ft_isnumber(*av))
@@ -70,9 +73,44 @@ t_stacks	ft_store_numbers(int size, char **av)
 		number = ft_atoi(*av);
 		if (!ft_isduplicate(&stacks, number))
 			exit(ft_put_err(&stacks, ft_strjoin(ft_strjoin("Error: ", *av), " is duplicated"), 0));
-		stacks.stack_a[i++] = ft_atoi(*av++);
+		stacks.stack_a.vector[i++] = ft_atoi(*av++);
+		stacks.stack_a.used_size++;
 	}
 	return (stacks);
+}
+
+void	ft_print_stack_a(t_stacks *stacks)
+{
+	int i;
+
+	i = 0;
+	printf("/*****************************stack_a****************************/\n");
+	while (i < stacks->stack_a.used_size)
+	{
+		printf("|size of stack_a= %d|used size = %d|value = %d|\n",stacks->stack_a.size, stacks->stack_a.used_size,  stacks->stack_a.vector[i]);
+		i++;
+	}
+}
+
+void	ft_print_stack_b(t_stacks *stacks)
+{
+	int i;
+
+	printf("/*****************************stack_b****************************/\n");
+	i = 0;
+	while (i < stacks->stack_b.used_size)
+	{
+		printf("|size of stack_b = %d|used size = %d|value = %d|\n",stacks->stack_b.size, stacks->stack_b.used_size, stacks->stack_b.vector[i]);
+		i++;
+	}
+}
+
+void	ft_print(t_stacks *stacks)
+{
+	int i;
+
+	ft_print_stack_a(stacks);
+	ft_print_stack_b(stacks);
 }
 
 int main(int ac, char **av)
@@ -82,10 +120,22 @@ int main(int ac, char **av)
 	int			i;
 	// create function gets the numbers in the arguments and stroe them in arr
 	stacks = ft_store_numbers(ac - 1, av + 1);
-	i = 0;
-	while (i < stacks.arr_size)
-		printf("%d|%d|\n",ac, stacks.stack_a[i++]);
-	free(stacks.stack_a);
-	free(stacks.stack_b);
+	ft_print(&stacks);
+	//ft_swap_a(&stacks, YES);
+	ft_push_b(&stacks, YES);
+	ft_push_b(&stacks, YES);
+	ft_push_b(&stacks, YES);
+	ft_push_b(&stacks, YES);
+	ft_push_b(&stacks, YES);
+	// printf("after swaping\n");
+	// ft_print(&stacks);
+	// ft_push_a(&stacks, YES);
+	// ft_push_a(&stacks, YES);
+	// ft_push_a(&stacks, YES);
+	// ft_print(&stacks);
+	ft_print_stack_b(&stacks);
+	ft_rotate_b(&stacks.stack_b, YES);
+	ft_print_stack_b(&stacks);
+	
 	return (ft_put_err(&stacks, NULL, 0));
 }
--- push_swap.h
+++ push_swap.h
@@ -6,15 +6,35 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 20:54:59 by mbari             #+#    #+#             */
-/*   Updated: 2021/06/08 20:55:06 by mbari            ###   ########.fr       */
+/*   Updated: 2021/06/09 21:04:24 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
+#define	YES 1
+#define	NO 0
+
+
+typedef struct	s_vector
+{
+	int	*vector;
+	int size;
+	int used_size;
+}				t_vector;
+
 typedef struct 	s_stacks
 {
-	int	*stack_a;
-	int	*stack_b;
-	int	arr_size;
+	t_vector	stack_a;
+	t_vector	stack_b;
 }				t_stacks;
+
+
+
+void	ft_swap_a(t_stacks *stacks, int print);
+void	ft_swap_b(t_stacks *stacks, int print);
+void	ft_swap_both(t_stacks *stacks);
+void	ft_push_a(t_stacks *stacks, int print);
+void	ft_push_b(t_stacks *stacks, int print);
+void	ft_rotate_a(t_vector *stack_a, int print);
+void	ft_rotate_b(t_vector *stack_b, int print);
