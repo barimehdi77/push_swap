508d046977ed0cb6943b284a02b2de00d7ee9e9e
barimehdi77 <barimehdi777@gmail.com>
2021-06-14 20:33:10 +0100

    upgrade script

--- script.bash
+++ script.bash
@@ -3,11 +3,11 @@
 
 while [ 1 ]
 do 
-	rand=`ruby -e "puts (0..4).to_a.shuffle.join(' ')"`;
+	rand=`ruby -e "puts ($1..$2).to_a.shuffle.join(' ')"`;
 	num=$(./a.out $rand | grep -E "^(sa|pa|ra|sb|pb|rb|rra|rrb)" | wc -l);
-	if [ $num -gt $1 ]
+	if [ $num -gt $3 ]
 	then
-		echo "ERROR: |$rand| sorted in '$num' instead of '$1'"
+		echo "ERROR: |$rand| sorted in '$num' instead of '$3'"
 		exit
 	fi
 	echo "|$rand| sorted in : $num"
