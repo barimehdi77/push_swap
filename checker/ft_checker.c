0b906d43b3fa03e3550f283be0b05edc82a596bd
barimehdi77 <barimehdi777@gmail.com>
2021-06-22 17:20:17 +0100

    add main.c for checker file

--- checker/ft_checker.c
+++ checker/ft_checker.c
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 09:35:44 by mbari             #+#    #+#             */
-/*   Updated: 2021/06/22 16:58:13 by mbari            ###   ########.fr       */
+/*   Updated: 2021/06/22 17:19:38 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -230,27 +230,4 @@ void	ft_select_instruction(char *line, t_stacks *stacks)
 		ft_push_b(stacks);
 	else
 		exit(ft_put_err(stacks, "Error", 0));
-}
-
-int main(int ac, char **av)
-{
-	char	*line;
-	t_stacks	stacks;
-	char	*string;
-	int ret;
-	
-	ac--;
-	string = ft_av_to_string(av);
-	stacks = ft_store_numbers(string);
-	// ft_print(&stacks);
-	line = NULL;
-	ret = 0;
-	// ft_print(&stacks);
-	while (get_next_line(0, &line) == 1)
-	{
-		ft_select_instruction(line, &stacks);
-		// ft_putendl_fd(line, 1);
-		free(line);
-	}
-	ft_print(&stacks);
 }
